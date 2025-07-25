2755271d0fac85e6a7939f2615aae7fd13f216ff
UnidentifiedAccount <tanthaddeo5@gmail.com>
2025-07-25 05:06:16 +0000

    dino

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,12 +10,19 @@ export class Game extends Scene {
     }
 
     preload() {
+        //load assets
+        
+        this.load.spritesheet("dino", "assets/dino-run.png",{frameWidth:88, frameHeight:94})
     }
 
     create() {
+        //initialise everything when game starts
+        this.physics.add.image(200,200, "dino")
+            .set0rigin(0);
     }
 
     update() {
+        //game logic
     }
 
 }
